750904998d0edebacdd344ce6e112ee824500aa3
normohammad <noor.mohmd27@gmail.com>
2017-07-03 14:37:18 +0200

    js>main.js>put variable for ordi and user

---
+++ js/main.js
@@ -0,0 +1,45 @@
+function game() {
+  var x;
+  var y =["paper", "ciseaux", "stone"];
+  var ordi =0;
+  var z;
+
+  // choix ordi
+  // function choixOrdi(){
+  //   z = Math.floor((Math.random() * 3));
+  //   ordi = y[z];
+  // }
+
+
+  choixOrdi();
+  // choix user
+  x =prompt("game");
+
+  while (x == ordi){
+    alert("equal");
+    choixOrdi();
+    x =prompt("game");
+  }
+
+
+    if (x ==="paper" && ordi ===y[2]){
+      alert("you win");
+    }
+
+    else if (x ==="ciseaux" && ordi ===y[0]){
+      alert("you win");
+    }
+    else if (x ==="ciseaux"  && ordi ===y[2]){
+      alert("you lost");
+    }
+
+    else if (x ==="paper" && ordi ===y[1]){
+      alert("you lost");
+    }
+    else if (x === "stone" && ordi ===y[0]){
+      alert("you lost");
+    }
+    else if (x ==="stone" && ordi ===y[1]){
+      alert("you win");
+    }
+}
